15772f928b5d25b0dafa0a474aa2f4059b93f2f5
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-09-18 23:38:32 +0530

    fixed market cap

--- contracts/quasar.sol
+++ contracts/quasar.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract Quasar is ERC20{
-
-    constructor(uint256 initialSupply) ERC20("Quasar","QUA"){
-        _mint(msg.sender);
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+contract Quasar is ERC20Capped {
+    // address payable public owner;
+    constructor(uint256 cap) ERC20("Quasar ", "QUA")  ERC20Capped(cap * (10** decimals())){ 
+        // owner = payable(msg.sender);
+        // declaring initial supply of 50 million * 10^18 wei
+        _mint(payable(msg.sender), 56000000 * (10 ** decimals()));
     }
-
 }
