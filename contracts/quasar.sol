c892849c24e2b9946e417d1c9ea1b45d63e8dee0
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-09-18 23:47:49 +0530

    fine

--- contracts/quasar.sol
+++ contracts/quasar.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-contract Quasar is ERC20Capped {
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+contract Quasar is ERC20Capped, ERC20Burnable {
     // address payable public owner;
     constructor(uint256 cap) ERC20("Quasar ", "QUA")  ERC20Capped(cap * (10** decimals())){ 
         // owner = payable(msg.sender);
