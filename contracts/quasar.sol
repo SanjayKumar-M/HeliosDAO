d23c2229d9616e90779eb7955ab2055a8e306f4e
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-09-19 09:45:22 +0530

    _mintminerreward function

--- contracts/quasar.sol
+++ contracts/quasar.sol
@@ -6,10 +6,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 contract Quasar is ERC20Capped, ERC20Burnable {
-    // address payable public owner;
+    address payable public owner;
+    uint256 public blockReward;
     constructor(uint256 cap) ERC20("Quasar ", "QUA")  ERC20Capped(cap * (10** decimals())){ 
-        // owner = payable(msg.sender);
+        owner = payable(msg.sender);
         // declaring initial supply of 50 million * 10^18 wei
-        _mint(payable(msg.sender), 56000000 * (10 ** decimals()));
+        _mint(owner, 56000000 * (10 ** decimals()));
     }
+
+    modifier onlyOwner{
+        require(msg.sender == owner,"Permission Denied!");
+        _;
+    }
+
+    function setBlockReward(uint56 reward) public onlyOwner{
+        blockReward=  reward *(10 ** decimals());
+
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(address from, address to,uint256 amount) internal virtual override{
+
+        
+    }   
 }
