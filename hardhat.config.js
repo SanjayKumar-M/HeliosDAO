3eae8b96187b813779bc1f203b84f5141837d31e
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-11-05 16:41:12 +0530

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,15 @@ module.exports = {
   solidity: "0.8.19",
   
   networks:{
-    polygon_mumbai:{
+    mumbai:{
       url:process.env.API_KEY,
       
       accounts:[process.env.PRIVATE_KEY]
+    },
+    xinfin:{
+      url:process.env.NETWORK_URL,
+      accounts:[process.env.THE_PRIVATE_KEY]
     }
   }
 };
+  
