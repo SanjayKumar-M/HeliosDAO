2e3df9ad5022bafa4c6b3ea898935ba4743c0988
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-11-07 22:31:20 +0530

    ok

--- contracts/HeliosCoin.sol
+++ contracts/HeliosCoin.sol
@@ -1,38 +1,38 @@
-//SPDX-License-Identifier: MIT
+    //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+    pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+    import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+    import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract HeliosCoin is ERC20{
+    contract HeliosCoin is ERC20{
 
-    uint256  _initalsupply;
-    
-    mapping(address =>uint256 ) balances;
-    mapping(address => mapping (address=> uint256)) allowed;
-    // event Transfer(address indexed from, address indexed to, uint256 value);
-    
+        uint256  _initalsupply;
+        
+        mapping(address =>uint256 ) balances;
+        mapping(address => mapping (address=> uint256)) allowed;
+        // event Transfer(address indexed from, address indexed to, uint256 value);
+        
 
-    constructor(uint256 initalSupply) ERC20("HeliosCoin","HLOS"){
-        _initalsupply = initalSupply;
-        balances[msg.sender]= _initalsupply;
+        constructor(uint256 initalSupply) ERC20("HeliosCoin","HLOS"){
+            _initalsupply = initalSupply;
+            balances[msg.sender]= _initalsupply;
 
+        }
+
+        function totalSupply()public override  view returns (uint256) {
+            return _initalsupply;
+
+        }
+        function balanceOf(address tokenOwner) public override  view returns (uint){
+            return balances[tokenOwner];
+
+        }
+        function transfer(address reciever,uint256 Amount) public override  returns (bool){
+            balances[msg.sender] = balances[msg.sender] - Amount;
+            balances[reciever] += Amount;
+            emit Transfer(msg.sender, reciever, Amount);
+            return true; 
+        }
+        
     }
-
-    function totalSupply()public override  view returns (uint256) {
-        return _initalsupply;
-
-    }
-    function balanceOf(address tokenOwner) public override  view returns (uint){
-        return balances[tokenOwner];
-
-    }
-    function transfer(address reciever,uint256 Amount) public override  returns (bool){
-        balances[msg.sender] = balances[msg.sender] - Amount;
-        balances[reciever] += Amount;
-        emit Transfer(msg.sender, reciever, Amount);
-        return true; 
-    }
-    
-}
