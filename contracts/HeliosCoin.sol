40e18c1833a8d43069ac15e7d4f8e0d8b3133f8d
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-11-11 00:51:19 +0530

    deployed

--- contracts/HeliosCoin.sol
+++ contracts/HeliosCoin.sol
@@ -2,54 +2,54 @@
 
 pragma solidity ^0.8.19;
 
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
 
 contract HeliosCoin is ERC20{
 
-        uint256  _initalsupply;
-        
-        mapping(address =>uint256 ) balances;
-        mapping(address => mapping (address=> uint256)) allowed;
-        // event Transfer(address indexed from, address indexed to, uint256 value);
-        
+   uint256  _initalsupply;
+    
+    mapping(address =>uint256 ) balances;
+    mapping(address => mapping (address=> uint256)) allowed;
+    // event Transfer(address indexed from, address indexed to, uint256 value);
+    
 
-        constructor(uint256 initalSupply) ERC20("HeliosCoin","HLOS"){
-            _initalsupply = initalSupply;
-            balances[msg.sender]= _initalsupply;
+    constructor(uint256 initalSupply) ERC20("HeliosCoin","HLOS"){
+        _initalsupply = initalSupply;
+        balances[msg.sender]= _initalsupply;
 
-        }
+    }
 
-        function totalSupply()public override  view returns (uint256) {
-            return _initalsupply;
+    function totalSupply()public override  view returns (uint256) {
+        return _initalsupply;
 
-        }
-        function balanceOf(address tokenOwner) public override  view returns (uint){
-            return balances[tokenOwner];
+    }
+    function balanceOf(address tokenOwner) public override  view returns (uint){
+        return balances[tokenOwner];
 
-        }
-        function transfer(address reciever,uint256 Amount) public override  returns (bool){
-            balances[msg.sender] = balances[msg.sender] - Amount;
-            balances[reciever] += Amount;
-            emit Transfer(msg.sender, reciever, Amount);
-            return true; 
-        }
-         function transferFrom(address from,address to,uint256 value) public override returns(bool) {
+    }
+    function transfer(address reciever,uint256 Amount) public override  returns (bool){
+        balances[msg.sender] = balances[msg.sender] - Amount;
+        balances[reciever] += Amount;
+        emit Transfer(msg.sender, reciever, Amount);
+        return true; 
+    }
+
+    function transferFrom(address from,address to,uint256 value) public override returns(bool) {
             balances[from] -= value;
             balances[to] += value;
             allowed[msg.sender][from] -= value;
             return true;
 
-        }
-
-        function approve(address spender,uint256 value) public override returns(bool){
+    }
+    function approve(address spender,uint256 value) public override returns(bool){
         allowed[spender][msg.sender] = value;
         return true;
-        }
-
-        function allowance(address owner,address spender)public override view returns(uint256){
+    }
+    function allowance(address owner,address spender)public override view returns(uint256){
         return allowed[spender][owner];
 
     }
-        
 }
---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat")
+const main = async() =>{
+
+    const HeliosCoin = await hre.ethers.getContractFactory("HeliosCoin");
+    const deployContract = await HeliosCoin.deploy("20000000") ;
+    await deployContract.waitForDeployment();
+    const address = deployContract.address;
+    console.log(`Contract deployed successfully to ${address}`);
+     
+
+}
+
+main().catch((err)=>{console.log(err)})
+
+
+//Token address => 0x27075464e8238a0FB3deD56d3dBba753DD2882F4
+//contract address => 0x27075464e8238a0FB3deD56d3dBba753DD2882F4
